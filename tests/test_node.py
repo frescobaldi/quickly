1acd84580cf3c9d9e6c9c29469fecf582e19b21d
Wilbert Berendsen <wbsoft@xs4all.nl>
2020-06-03 14:00:01 +0200

    start pytest testing suite

---
+++ tests/test_node.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of `quickly`, a library for LilyPond and the `.ly` format
+#
+# Copyright © 2019-2020 by Wilbert Berendsen <info@wilbertberendsen.nl>
+#
+# This module is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This module is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+"""
+Test the node module.
+"""
+
+### find quickly
+import sys
+sys.path.insert(0, '.')
+
+from quickly.node import Node
+
+
+class N1(Node):
+    pass
+
+
+class N2(Node):
+    pass
+
+
+class N3(Node):
+    pass
+
+
+class M1(N1):
+    pass
+
+
+class M2(N2):
+    pass
+
+
+class M3(N3):
+    pass
+
+
+tree = \
+N1(
+    N2(
+        N3(),
+        M3(),
+        N2(),
+        M1(),
+    ),
+    N1(
+        M2(),
+    ),
+)
+
+
+def test_main():
+    assert next(tree//M3) is tree[0][1]
+    assert len(list(tree/N2)) == 1
+    assert sum(1 for _ in tree//N2) == 3     # M2 inherits from N2 :-)
+    assert next(tree[1][0] << N1) is tree[1]
+    tree2 = tree.copy()
+    assert tree.equals(tree2)
+    tree2[0][3] = N1()
+    assert not tree.equals(tree2)
+    assert tree[0][2].common_ancestor(tree[1][0]) is tree
+    assert tree[0][2].common_ancestor(tree2[1][0]) is None
+
+
+
+if __name__ == "__main__" and 'test_main' in globals():
+    test_main()
+
---
+++ tests/test_template.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of `quickly`, a library for LilyPond and the `.ly` format
+#
+# Copyright © 2019-2020 by Wilbert Berendsen <info@wilbertberendsen.nl>
+#
+# This module is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This module is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+"""
+Template for test files that test quickly.
+"""
+
+### find quickly
+import sys
+sys.path.insert(0, '.')
+
+import quickly
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__" and 'test_main' in globals():
+    test_main()
