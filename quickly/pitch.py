e3a76bf0beb3c2af9530449d376bc5793c2ebced
Wilbert Berendsen <wbsoft@xs4all.nl>
2021-12-18 12:09:53 +0100

    Improve pitch functionality

--- quickly/pitch.py
+++ quickly/pitch.py
@@ -19,7 +19,7 @@
 
 
 """
-Functions to deal with LilyPond pitches.
+Classes and functions to deal with LilyPond pitches.
 """
 
 
@@ -35,55 +35,179 @@ def _make_reverse_pitch_table():
         notes = collections.defaultdict(list)
         for name, note_alter in pitches.items():
             notes[note_alter].append(name)
-        yield language, dict(notes)
+        yield language, {note_alter: tuple(names)
+                for note_alter, names in notes.items()}
 
 pitch_names_reversed = dict(_make_reverse_pitch_table())
 del _make_reverse_pitch_table
 
 
-class PitchNameWriter:
-    """Write pitch names in all LilyPond languages.
+class PitchNameProcessor:
+    """Read and write pitch names in all LilyPond languages.
+
+    The language to use by default can be set in the ``language`` attribute;
+    you can also specify the language on every call to the :meth:`read` or
+    :meth:`write` method.
 
     Some languages have multiple pitch names for the same note; using the
-    ``prefer_`` attributes you can control which style is chosen.
+    ``prefer_`` attributes you can control which style is chosen on
+    :meth:`write`.
 
     """
-    prefer_long = False         #: prefer long names in english, e.g. c-sharpsharp above css
-    prefer_accented = False     #: prefer ré above re (in francais)
-    prefer_x = False            #: prefer dox above doss, cx above css, etc in enspanol, english, francais
-    prefer_double_s = False     #: prefer ss above s in norsk
-    prefer_classic = True       #: prefer es above ees and as above aes (in nederlands, norsk)
-    prefer_deprecated = False   #: prefer names marked as deprecated
+    #: Prefer long names in english, e.g. ``c-sharpsharp`` above ``css``
+    prefer_long = False
 
-    def write(self, note, alter=0, language="nederlands"):
+    #: Prefer ``ré`` above ``re`` (in francais)
+    prefer_accented = False
+
+    #: Prefer ``dox`` above ``doss``, ``cx`` above ``css``, etc in enspanol, english, francais
+    prefer_x = False
+
+    #: Prefer ``ss`` above ``s`` inside note names in norsk
+    prefer_double_s = False
+
+    #: Prefer ``es`` above ``ees`` and ``as`` above ``aes`` (in nederlands, norsk)
+    prefer_classic = True
+
+    #: Prefer names marked as deprecated
+    prefer_deprecated = False
+
+    _language = "nederlands"
+
+    @property
+    def language(self):
+        return self._language
+
+    @language.setter
+    def language(self, language):
+        if language not in pitch_names:
+            raise KeyError("unknown language name")
+        self._language = language
+
+    @language.deleter
+    def language(self):
+        self._language = "nederlands"
+
+    language.__doc__ = \
+        """The language to use if none is specified (default: ``"nederlands"``).
+
+        Deleting this attribute sets it back to ``"nederlands"``.
+
+        Raises a :obj:`KeyError` if the language you try to set does not exist.
+        Valid languages are: {langs}.
+
+        """.format(langs = ", ".join('``"{}"``'.format(name)
+            for name in sorted(pitch_names)))
+
+    def read(self, name, language=None):
+        """Returns ``(note, alter)`` for the specified note name.
+
+        The default ``language`` is used if you don't specify one.
+
+        Raises a :obj:`KeyError` if the language does not know the pitch name,
+        or when the language name is unknown.
+
+        For example::
+
+            >>> from quickly.pitch import PitchNameProcessor
+            >>> p=PitchNameProcessor()
+            >>> p.read('cis')
+            (0, 0.5)
+
+        """
+        return pitch_names[language or self._language][name]
+
+    def write(self, note, alter=0, language=None):
         """Return a pitch name for the specified note and alter.
 
         The ``note`` is a value in the range 0..6, and ``alter`` a value
         between -1 and 1. Most used alterations are -1, -0.5, 0, 0.5 and 1, but
         some languages also support quarter tones, like 0.25.
 
-        Raises a KeyError if the language does not contain a pitch name, or
-        when the language name is unknown.
+        The default ``language`` is used if you don't specify one.
+
+        Raises a :obj:`KeyError` if the language does not contain a pitch name,
+        or when the language name is unknown.
 
         For example::
 
-            >>> from quickly.pitch import PitchNameWriter
-            >>> w=PitchNameWriter()
-            >>> w.write(0)
+            >>> from quickly.pitch import PitchNameProcessor
+            >>> p=PitchNameProcessor()
+            >>> p.write(0)
             'c'
-            >>> w.write(4, 1)
+            >>> p.write(4, 1)
             'gisis'
-            >>> w.write(4, 1, 'english')
+            >>> p.write(4, 1, 'english')
             'gss'
-            >>> w.prefer_long = True
-            >>> w.write(4, 1, 'english')
+            >>> p.prefer_long = True
+            >>> p.write(4, 1, 'english')
             'g-sharpsharp'
 
         """
-        names = pitch_names_reversed[language][note, alter]
+        names = pitch_names_reversed[language or self._language][note, alter]
         if len(names) == 1:
             return names[0]
         return self._suitable(language, names) or names[-1]
+
+    def distill_preferences(self, pitchnames, language=None):
+        """Iterate over the ``pitchnames`` and try to distill the preferred style.
+
+        Adjust the preferences based on the encountered pitches.
+        The default ``language`` is used if you don't specify one.
+
+        """
+        if not language:
+            language = self._language
+
+        prefer_accented = None
+        prefer_classic = None
+        prefer_double_s = None
+        prefer_long = None
+        prefer_x = None
+        prefer_deprecated = None
+
+        names = pitch_names[language]
+        for name in pitchnames:
+            if name in names:
+                if 'é' in name:
+                    prefer_accented = True
+                elif not prefer_accented and language.startswith('fran') and 'e' in name:
+                    prefer_accented = False
+                if '-' in name:
+                    prefer_long = True
+                elif not prefer_long and language == "english" and names[name][1] in {-1, -0.5, 0.5, 1}:
+                    prefer_long = False
+                elif language == "norsk" and 'ss' in name:
+                    prefer_double_s = True
+                elif not prefer_double_s and language == "norsk" and 's' in name:
+                    prefer_double_s = False
+                elif language == "nederlands" and name in {'es', 'eses', 'as', 'ases'}:
+                    prefer_classic = True
+                elif not prefer_classic and language == "nederlands" and name in {'ees', 'eeses', 'aes', 'aeses'}:
+                    prefer_classic = False
+                elif language == "deutsch" and name in {'eeh', 'ases', 'aseh', 'aeh'}:
+                    prefer_deprecated = True
+                elif not prefer_deprecated and language == "deutsch" and name in {'eh', 'asas', 'asah', 'ah'}:
+                    prefer_deprecated = False
+                elif name.endswith('x'):
+                    prefer_x = True
+                elif not prefer_x and (
+                           (name.endswith('ss') and language in ("english", "espanol", "español"))
+                        or (name.endswith('dd') and language.startswith("fran"))):
+                    prefer_x = False
+
+        if prefer_accented is not None:
+            self.prefer_accented = prefer_accented
+        if prefer_classic is not None:
+            self.prefer_classic =prefer_classic
+        if prefer_double_s is not None:
+            self.prefer_double_s = prefer_double_s
+        if prefer_long is not None:
+            self.prefer_long = prefer_long
+        if prefer_x is not None:
+            self.prefer_x = prefer_x
+        if prefer_deprecated is not None:
+            self.prefer_deprecated = prefer_deprecated
 
     _suitable = parce.util.Dispatcher()
 
@@ -102,7 +226,7 @@ class PitchNameWriter:
     @_suitable("deutsch")
     def _deutsch(self, names):
         for name in names:
-            if self.prefer_deprecated != (name in {'eh', 'asas', 'asah', 'ah'}):
+            if self.prefer_deprecated == (name in {'eeh', 'ases', 'aseh', 'aeh'}):
                 return name
 
     @_suitable("english")
@@ -112,37 +236,35 @@ class PitchNameWriter:
                 if "-" in name:
                     return name
         for name in names:
-            if "-" not in name and self.prefer_x == (name.endswith('x')):
+            if "-" not in name and self.prefer_x == name.endswith('x'):
                 return name
 
     @_suitable("espanol")
     @_suitable("español")
     def _espanol(self, names):
         for name in names:
-            if self.prefer_x == (name.endswith('x')):
+            if self.prefer_x == name.endswith('x'):
                 return name
 
     @_suitable("francais")
     @_suitable("français")
     def _francais(self, names):
-        if self.prefer_accented:
-            accented = [name for name in names if 'é' in name]
-            if accented:
-                if len(accented) == 1:
-                    return accented[0]
-                names = accented
+        subset = [name for name in names if self.prefer_accented == ('é' in name)]
+        if subset:
+            if len(subset) == 1:
+                return subset[0]
+            names = subset
         for name in names:
             if self.prefer_x == (name.endswith('x')):
                 return name
 
     @_suitable("norsk")
     def _norsk(self, names):
-        if self.prefer_double_s:
-            double_s = [name for name in names if 'ss' in name]
-            if double_s:
-                if len(double_s) == 1:
-                    return double_s[0]
-                names = double_s
+        subset = [name for name in names if self.prefer_double_s ==  ('ss' in name)]
+        if subset:
+            if len(subset) == 1:
+                return subset[0]
+            names = subset
         for name in names:
             if self.prefer_classic == ('ee' in name or 'ae' in name):
                 return name
@@ -162,9 +284,7 @@ def num_to_octave(n):
     uses -1 for the octave without a ``'`` or ``,``.
 
     """
-    if n < 0:
-        return "," * -n
-    return "'" * n
+    return "," * -n if n < 0 else "'" * n
 
 
 def octave_to_num(octave):
---
+++ tests/test_pitch.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of `quickly`, a library for LilyPond and the `.ly` format
+#
+# Copyright © 2019-2020 by Wilbert Berendsen <info@wilbertberendsen.nl>
+#
+# This module is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This module is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+"""
+Template for test files that test quickly.
+"""
+
+import pytest
+
+### find quickly
+import sys
+sys.path.insert(0, '.')
+
+import quickly
+from quickly.pitch import *
+
+
+
+def test_main():
+    """Main test function."""
+
+    p = PitchNameProcessor()
+    assert 'c' == p.write(0)
+    assert 'cis' == p.write(0, 0.5)
+
+    assert 'cs' == p.write(0, 0.5, 'english')
+    p.prefer_long = True
+    assert 'c-sharp' == p.write(0, 0.5, 'english')
+
+    p.prefer_accented = True
+    assert 'ré' == p.write(1, 0, 'francais')
+    assert 'ré' == p.write(1, 0, 'français')
+
+    p.prefer_x = True
+    assert 'réx' == p.write(1, 1, 'francais')
+    p.prefer_x = False
+    assert 'rédd' == p.write(1, 1, 'francais')
+    p.prefer_accented = False
+    p.prefer_x = True
+    assert 'rex' == p.write(1, 1, 'francais')
+    p.prefer_x = False
+    assert 'redd' == p.write(1, 1, 'francais')
+
+    with pytest.raises(KeyError):
+        p.language = "does_not_exist"
+
+    p.language = "english"
+    assert 'c-sharp' == p.write(0, 0.5)
+
+    del p.language
+    assert p.language == "nederlands"
+    assert p.read('dis') == (1, 0.5)
+
+    assert p.read('réx', 'francais') == (1, 1)
+    assert p.read('rex', 'francais') == (1, 1)
+    assert p.read('rédd', 'francais') == (1, 1)
+    assert p.read('redd', 'francais') == (1, 1)
+
+    # distill prefs
+    w = PitchNameProcessor()
+    w.distill_preferences(['es', 'g'])
+    assert w.prefer_classic == True
+    w.distill_preferences(['ees', 'g'])
+    assert w.prefer_classic == False
+
+    # a non-specific note does not change the pref
+    w.distill_preferences(['fis', 'g'])
+    assert w.prefer_classic == False
+    w.prefer_classic = True
+    w.distill_preferences(['fis', 'g'])
+    assert w.prefer_classic == True
+
+    # one note changes two prefs
+    w = PitchNameProcessor()
+    w.language = "francais"
+    w.prefer_x = False
+    w.prefer_accented = False
+    w.distill_preferences(['réx'])
+    assert w.prefer_x == True
+    assert w.prefer_accented == True
+
+    # unaccented pref set back
+    w.distill_preferences(['red'])
+    assert w.prefer_x == True
+    assert w.prefer_accented == False
+
+    # x pref set back
+    w.distill_preferences(['redd'])
+    assert w.prefer_x == False
+    assert w.prefer_accented == False
+
+
+    # other functions
+    assert num_to_octave(3) == "'''"
+    assert num_to_octave(-3) == ",,,"
+    assert octave_to_num(",") == -1
+    assert octave_to_num("''") == 2
+    assert octave_to_num("',") == 0
+
+
+if __name__ == "__main__" and 'test_main' in globals():
+    test_main()
