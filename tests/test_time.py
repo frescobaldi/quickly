fd8d428ae25d355d4a9e70e6b7e675fe3509c5a0
Wilbert Berendsen <wbsoft@xs4all.nl>
2022-01-15 12:30:07 +0100

    time module test

--- tests/test_time.py
+++ tests/test_time.py
@@ -26,15 +26,39 @@ Template for test files that test quickly.
 import sys
 sys.path.insert(0, '.')
 
-import quickly
 
+from fractions import Fraction
+
+import parce
+import quickly
+from quickly.time import Time
+from quickly.dom import lily, read
 
 
 
 def test_main():
     """Main test function."""
+    d = parce.Document(quickly.find('lilypond'), r'''
+music = { c4 d e f }
 
+{ c2 \music g a b8 g f d }
+''', transformer=True)
 
+    m = d.get_transform(True)
+    t = Time()
+    assert t.duration(m[1][0], m[1][2]).time == 2       # length of "c2 \music g" part (g has duration 2)
+
+    c = parce.Cursor(d, 44, 47)
+    assert t.cursor_position(c).time == Fraction(11, 4) # length or music before the cursor
+    assert t.cursor_duration(c).time == Fraction(1, 4)  # duration of the selected music
+
+    m = read.lily(r"\tuplet 3/2 { c8 d e }")
+    assert t.length(m) == Fraction(1, 4)
+    assert t.length(m[1][1]) == Fraction(1, 12)         # one note in tuplet
+
+    m = read.lily(r"\shiftDurations #1 #1 { c4 d e f }")
+    assert t.length(m) == Fraction(3, 4)        # note value halved and dot added, so should be 3/4
+    assert t.length(m[2][2]) == Fraction(3, 16) # autodiscovers the current duration transform
 
 
 
