9546264826b420e533fbbfb15bd42a07481ba0d8
Wilbert Berendsen <wbsoft@xs4all.nl>
2022-03-13 16:30:09 +0100

    ver bump and upd chlog

--- quickly/pkginfo.py
+++ quickly/pkginfo.py
@@ -35,7 +35,7 @@ Version = collections.namedtuple("Version", "major minor patch")
 name = "quickly"
 
 #: the current version
-version = Version(0, 6, 0)
+version = Version(0, 7, 0)
 version_suffix = ""
 #: the current version as a string
 version_string = "{}.{}.{}".format(*version) + version_suffix
